8dbc3464dae14dced6b133ff8ccf26d2d2e31aba
Maverick <supacode@gmail.com>
2021-06-20 05:29:07 +0100

    Convert to Typescript

---
+++ src/components/header.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+export const Header: React.FC<{ siteTitle: string }> = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+);
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+};
+
+Header.defaultProps = {
+  siteTitle: ``
+};
