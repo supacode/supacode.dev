0cc169c000bfe31a657d3810ae8caa0c1397d663
Restyled.io <commits@restyled.io>
2021-06-23 05:07:22 +0000

    Restyled by prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,46 @@
 module.exports = {
-  siteMetadata : {
-    title : `Gatsby Default Starter`,
-    description :
-        `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author : `@gatsbyjs`,
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
-  plugins : [
+  plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
     {
-      resolve : `gatsby-source-filesystem`,
-      options : {
-        name : `images`,
-        path : `${__dirname}/src/images`,
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve : `gatsby-plugin-webfonts`,
-      options : {
-        fonts : {
-          google : [
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
             {
-              family : 'Roboto Mono',
-              variants : [ '300', '400', '500', '600', '700' ],
+              family: 'Roboto Mono',
+              variants: ['300', '400', '500', '600', '700'],
             },
           ],
         },
       },
     },
     {
-      resolve : `gatsby-plugin-manifest`,
-      options : {
-        name : `gatsby-starter-default`,
-        short_name : `starter`,
-        start_url : `/`,
-        background_color : `#663399`,
-        theme_color : `#663399`,
-        display : `minimal-ui`,
-        icon : `src/images/gatsby-icon.png`, // This path is relative to the
-                                             // root of the site.
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the
+        // root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
