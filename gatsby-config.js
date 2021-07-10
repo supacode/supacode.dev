2c912b2c99f3035777529f521323e480c0d6c7aa
Restyled.io <commits@restyled.io>
2021-07-10 04:14:29 +0000

    Restyled by prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,45 @@
 module.exports = {
-  siteMetadata : {
-    title : `Maverick`,
-    description : `Maverick's Portfolio`,
-    author : `@supacode`,
+  siteMetadata: {
+    title: `Maverick`,
+    description: `Maverick's Portfolio`,
+    author: `@supacode`,
   },
-  plugins : [
+  plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
     {
-      resolve : `gatsby-source-filesystem`,
-      options : {
-        name : `images`,
-        path : `${__dirname}/src/images`,
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve : `gatsby-plugin-webfonts`,
-      options : {
-        fonts : {
-          google : [
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
             {
-              family : 'Roboto Mono',
-              variants : [ '300', '400', '500', '600', '700' ],
+              family: 'Roboto Mono',
+              variants: ['300', '400', '500', '600', '700'],
             },
           ],
         },
       },
     },
     {
-      resolve : `gatsby-plugin-manifest`,
-      options : {
-        name : `gatsby-starter-default`,
-        short_name : `starter`,
-        start_url : `/`,
-        background_color : `#663399`,
-        theme_color : `#663399`,
-        display : `minimal-ui`,
-        icon : `src/images/gatsby-icon.png`, // This path is relative to the
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the
         // root of the site.
       },
     },
