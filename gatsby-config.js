29e008041594f6cf5251b2c503bbefadb42b55c1
Restyled.io <commits@restyled.io>
2021-09-04 18:04:23 +0000

    Restyled by clang-format

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: `supacode | Maverick's Portfolio`,
-    description: ` Welcome to my little corner of the web, where I share notes, code snippets, and resources on topics that interest me.`,
-    author: `@supacode`,
-    twitterUsername: '@supacode',
-    siteUrl: 'https://www.supacode.dev',
-    image: '/src/images/supacode.png', // Path to your image you placed in the 'static' folder
+  siteMetadata : {
+    title : `supacode | Maverick's Portfolio`,
+    description :
+        ` Welcome to my little corner of the web, where I share notes, code snippets, and resources on topics that interest me.`,
+    author : `@supacode`,
+    twitterUsername : '@supacode',
+    siteUrl : 'https://www.supacode.dev',
+    image : '/src/images/supacode.png', // Path to your image you placed in the
+                                        // 'static' folder
   },
-  plugins: [
+  plugins : [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
@@ -15,29 +17,29 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        name : `images`,
+        path : `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/content/`,
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        name : `content`,
+        path : `${__dirname}/content/`,
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#1f2029`,
-        theme_color: ` #1f2029`,
-        display: `minimal-ui`,
-        icon: `src/images/supacode.png`, // This path is relative to the
+      resolve : `gatsby-plugin-manifest`,
+      options : {
+        name : `gatsby-starter-default`,
+        short_name : `starter`,
+        start_url : `/`,
+        background_color : `#1f2029`,
+        theme_color : ` #1f2029`,
+        display : `minimal-ui`,
+        icon : `src/images/supacode.png`, // This path is relative to the
         // root of the site.
       },
     },
