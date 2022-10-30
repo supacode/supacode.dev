02cafddd134a470ce685761a374332ba3a839555
Maverick <supacode@gmail.com>
2022-10-31 00:54:45 +0100

    Add site theme-color

--- modules/layout/AppLayout.tsx
+++ modules/layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { SiteFooter } from 'components/SiteFooter';
 import { Header } from 'modules/mainHeader/Header';
+import Head from 'next/head';
 
 type AppLayoutProps = {
   children: React.ReactNode;
@@ -8,6 +9,9 @@ type AppLayoutProps = {
 export const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
     <>
+      <Head>
+        <meta name="theme-color" content="#1a2130" />
+      </Head>
       <Header />
       <div className="app-container">
         {children}
