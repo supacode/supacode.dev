4f6ede546f633786fe7601e7a126559905677de3
Restyled.io <commits@restyled.io>
2022-12-09 17:02:49 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
-  dest: 'public',
+  dest : 'public',
 });
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode : true,
+  swcMinify : true,
 };
 
 module.exports = withPWA(nextConfig);
